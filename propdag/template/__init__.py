43f96eb97f5606337e18acb7119e358d7765f5cd
ZhongkuiMa <zhongkuima0419@gmail.com>
2025-12-27 10:36:12 +1000

    Export clear_bwd_cache and clear_fwd_cache from template module

--- propdag/template/__init__.py
+++ propdag/template/__init__.py
@@ -7,7 +7,7 @@ graph structures that support both forward and backward propagation.
 
 from propdag.template._arguments import TArgument
 from propdag.template._cache import TCache
-from propdag.template._model import TModel
+from propdag.template._model import TModel, clear_bwd_cache, clear_fwd_cache
 from propdag.template._node import TNode
 from propdag.template._sort import (
     topo_sort_backward,
@@ -20,6 +20,8 @@ __all__ = [
     "TCache",
     "TModel",
     "TNode",
+    "clear_bwd_cache",
+    "clear_fwd_cache",
     "topo_sort_backward",
     "topo_sort_forward_bfs",
     "topo_sort_forward_dfs",
